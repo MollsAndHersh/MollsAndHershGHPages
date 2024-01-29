5974408f8b67e97349b37145746e0e783eeb8d86
MollsAndHersh <mollsandhersh@gmail.com>
2024-01-29 14:31:16 -0700

    Added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), sitemap()],
   site: 'https://mollsandhersh.github.io',
   base: '/AstroIntegratedBuildTemplate',
 });
