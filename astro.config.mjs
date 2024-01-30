0aba6e989ef053b51603956897d2da19fd817b97
MollsAndHersh <mollsandhersh@gmail.com>
2024-01-29 17:26:04 -0700

    Added Astro Starlight Documentation Template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
@@ -6,7 +7,28 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), sitemap()],
-  site: 'https://mollsandhersh.github.io',
-  base: '/AstroIntegratedBuildTemplate',
+  integrations: [tailwind(), 
+    vue(), 
+    sitemap(),
+		starlight({
+			title: 'Articles',
+			social: {
+				github: 'https://github.com/mollsandhersh',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+  ],
+  site: 'https://mollsandhersh.github.io'
 });
