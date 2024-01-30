40d121cac7bb84b73b1ba5af49bf31a66ef4edd3
MollsAndHersh <mollsandhersh@gmail.com>
2024-01-29 18:18:31 -0700

    Added more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
 
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
@@ -10,25 +11,53 @@ export default defineConfig({
   integrations: [tailwind(), 
     vue(), 
     sitemap(),
-		starlight({
-			title: 'Articles',
-			social: {
-				github: 'https://github.com/mollsandhersh',
+	starlight({
+		plugins: [starlightLinksValidator()],
+		title: 'Site Search',
+		social: {
+			github: 'https://github.com/mollsandhersh',
+		},
+		// editLink: {
+		// 	baseUrl: 'https://github.com/withastro/contribute.docs.astro.build/edit/main/'
+		// },
+		customCss: ["./src/styles/theme.css"],
+		components: {
+			Head: './src/starlight-overrides/Head.astro',
+		},
+		logo: {
+			dark: './src/assets/logo-dark.svg',
+			light: './src/assets/logo-light.svg',
+			replacesTitle: true,
+		},
+		head: [
+		{
+			tag: 'script',
+			attrs: {
+				src: 'https://cdn.usefathom.com/script.js',
+				'data-site': 'EZBHTSIG',
+				defer: true,
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+		},
+		],
+		sidebar: [
+			{ 
+				label: 'Welcome', 
+				link: '/welcome/' 
+			},
+			{
+				label: 'Guides',
+				// items: [
+				// 	// Each item here is one entry in the navigation menu.
+				// 	{ label: 'Example Guide', link: '/guides/example/' },
+				// ],
+				autogenerate: { directory: 'guides' },
+			},
+			{
+				label: 'Reference',
+				autogenerate: { directory: 'reference' },
+			},
+		],
+	}),
   ],
   site: 'https://mollsandhersh.github.io'
 });
